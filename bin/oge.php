a3c5e3936cf334f702bb72496cc91ff5946f32a9
Kevin Klement <klement@umass.edu>
2023-07-06 21:14:06 -0400

    kron push 2023-07-06 21:14:06

--- bin/oge.php
+++ bin/oge.php
@@ -22,7 +22,75 @@ Usage:
 
 {$exename} [options] filename1 [filename2 ...]
 
+Options:
+
+--browser (executable)   : specify the browser to use
+                           (otherwise will read the \$BROWSER environmental
+                            variable, or what is set in settings.json
+                            falling back to "firefox")
+--help                   : view this help
+--host (domainname)      : specify a host domain other than "localhost"
+                           (can include '/'s for subdirectories)
+--port (NN)              : specify the port to look for the server
+                           (otherwise will be 8181 or what is
+                            set in settings.json)
+--templates (directory)  : if this is set, filenames of files that don’t
+                           exist already will be created according to
+                           templates with names of the form "ext.template"
+                           in the directory specified, e.g. "tex.template"
+                           for LaTeX file
+
+Default "browser", "host", "templates", and "port" can also be
+set in your settings.json file in the open-guide-editor directory.
 EOL;
+}
+
+// read the command line arguments
+$arg_to_read = 1;
+
+// initialize variables
+$browser = '';
+$host = '';
+$filenames = [];
+$port = 0;
+$templatesdir = '';
+
+while ($arg_to_read < count($argv)) {
+
+    $argument = $argv[$arg_to_read];
+
+    switch($argument) {
+
+        case '--browser':
+            if ($arg_to_read + 1 == count($argv)) {
+                exit('No browser specified');
+            }
+            $browser = $argv[$arg_to_read + 1];
+            $arg_to_read += 2;
+            break;
+
+        case '--port':
+            if ($arg_to_read + 1 == count($argv)) {
+                exit('No port specified');
+            }
+
+            $port = intval($argv[$arg_to_read + 1]);
+            $arg_to_read += 2;
+            break;
+    
+        case '-h';
+        case '-?';
+        case '--h';
+        case '-help';
+        case '--help':
+            showhelp();
+            exit(0);
+            break;
+        default:
+            $arg_to_read++;
+            break;
+    }
+
 }
 
 // move to the main open-guide-editor folder, which should be the
